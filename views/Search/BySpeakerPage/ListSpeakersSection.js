91d4fad35bcaef1ca41ee4704c26fa8fa4404b5b
miguelruor <miguelruiz98@outlook.com>
2021-05-08 20:47:03 -0500

    search by speakers connected to previous-talks/[id]

--- views/Search/BySpeakerPage/ListSpeakersSection.js
+++ views/Search/BySpeakerPage/ListSpeakersSection.js
@@ -2,28 +2,11 @@ import React, {useState} from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 import styles from "./BySpeakerPageStyle";
-
+import Link from 'next/link'
 import MenuOpen from '@material-ui/icons/MenuOpen';
-
-//import {db} from '../../../ConfigFirebase';
-import ReactHtmlParser from 'react-html-parser';
-
-// For modals
-
-import Button from "../../../components/CustomButtons/Button.js";
-import Dialog from "@material-ui/core/Dialog";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogActions from "@material-ui/core/DialogActions";
-import Close from "@material-ui/icons/Close";
-import IconButton from "@material-ui/core/IconButton";
-import Slide from "@material-ui/core/Slide";
+import Button from '../../../components/CustomButtons/Button'
 
 const useStyles = makeStyles(styles);
-
-const Transition = React.forwardRef(function Transition(props, ref) {
-    return <Slide direction="down" ref={ref} {...props} />;
-  });
 
 export default function ListSpeakersSection(props){
     const classes = useStyles();
@@ -47,19 +30,6 @@ export default function ListSpeakersSection(props){
     // al dar clic en una letra, se pone visitLetters[letter] = True, para mostrar la lista de speakers
     const [visitLetters, setVisitLetters] = useState({});
 
-    //modal activo o no
-    const [modal, setModal] = useState(false);
-    
-    // variables de platica del modal actual al dar clic en detalles de platica
-    const[talkTitle,setTalkTitle] = useState('');
-    const[talkDate,setTalkDate] = useState('');
-    const[talkDescription,setTalkDescription] = useState('');
-    const[talkVideo,setTalkVideo] = useState('');
-    const[talkPresentation,setTalkPresentation] = useState('');
-    const[talkSpeaker,setTalkSpeaker] = useState('');
-    const[talkKeywords,setTalkKeywords] = useState([]);
-    const[warningNote, setWarningNote] = useState('');
-
     // lista de speakers que empiezan con letter
     function listWithLetter(letter){
         const listItems = speakersListByLetter[letter].map(speaker => {
@@ -72,76 +42,13 @@ export default function ListSpeakersSection(props){
                     return (
                         <>
                             {firstTalk ? firstTalk=false  : ', ' }
-                            <Button 
-                                color='primary' 
-                                className={classes.buttonList}
-                                onClick = {() => {
-                                setModal(true);
-                                setTalkTitle(talks[talkID_aux].title);
-                                setTalkVideo(talks[talkID_aux].video);
-                                setTalkPresentation(talks[talkID_aux].slides);
-                                setTalkDescription(talks[talkID_aux].abstract);
-                                setTalkKeywords(talks[talkID_aux].keywords);
-                                setTalkSpeaker(speakersDic[talks[talkID_aux].speaker_id.toString()].completeName); 
-                                setTalkDate(talks[talkID_aux].date); 
-                                setWarningNote(talks[talkID_aux].warning);
-                            }}>
-                                {talks[talkID_aux].year}</Button>
-                            <Dialog
-                                classes={{
-                                root: classes.center,
-                                paper: classes.modal
-                                }}
-                                open={modal}
-                                TransitionComponent={Transition}
-                                keepMounted
-                                onClose={() => setModal(false)}
-                                aria-labelledby="modal-slide-title"
-                                aria-describedby="modal-slide-description"
-                            >
-                                <DialogTitle
-                                    id="classic-modal-slide-title"
-                                    disableTypography
-                                    className={classes.modalHeader}
+                            <Link href={'previous-talks/'+talkID_aux}>
+                                <Button 
+                                    color='primary' 
+                                    className={classes.buttonList}
                                 >
-                                    <IconButton
-                                        className={classes.modalCloseButton}
-                                        key="close"
-                                        aria-label="Close"
-                                        color="inherit"
-                                        onClick={() => {
-                                            setModal(false);
-                                        }
-                                    }
-                                    >
-                                        <Close className={classes.modalClose} />
-                                    </IconButton>
-                                    <h2 className={classes.modalTitle} >Talk Details</h2>
-                                </DialogTitle>
-                                <DialogContent
-                                    id="modal-slide-description"
-                                    className={classes.modalBody}
-                                    >
-                                    <p><b>Speaker: </b> {talkSpeaker} </p>
-                                    <p><b>Title: </b>{talkTitle} </p>
-                                    <p><b>Video: </b> {talkVideo === null ? 'Not available yet.' : <a href={talkVideo} target="_blank">Click here</a>} </p>
-                                    {/*Cuando una talk no tiene presentacion, talkSlides es undefined, y en otro caso string*/}
-                                    {typeof(talkPresentation) == "undefined" ? null : <><p><b>Slides:</b> <a href={talkPresentation} target="_blank">Click here</a></p></>}
-                                    {typeof(warningNote) == "undefined" ? null : <><p><b>Warning: </b>{warningNote}</p></>}
-                                    <p><b>Date: </b>{talkDate} </p>
-                                    <p><b>Keywords: </b> {talkKeywords.join(', ')}</p>
-                                    <p><b>Abstract: </b>{ReactHtmlParser (talkDescription)}</p>
-                                </DialogContent>
-                                <DialogActions className={classes.modalFooter}>
-                                <Button
-                                    onClick={() => setModal(false)}
-                                    color="danger"
-                                    simple
-                                >
-                                    Close
-                                </Button>
-                                </DialogActions>
-                            </Dialog>
+                                    {talks[talkID_aux].year}</Button>
+                            </Link>
                         </>
                     );  
                 })}
