58bfd3375c3053711641b76d92d524876daf0b09
Miguel Ruiz <miguelruiz98@outlook.com>
2022-11-02 16:05:13 -0600

    chore: delete unused import

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,20 @@
 import Head from 'next/head';
 import HomePage from '../views/HomePage/HomePage';
 import data from '../data/imageData.json';
-import speakers from '../data/speakers.json';
 import talks from '../data/talks.json';
 
-export async function getStaticProps(){
+export async function getStaticProps() {
   const totalImages = data.totalImages
 
   var images = []
 
-  for(var i=1; i<=totalImages; i+=1){
-    images.push('/img/imagesCarousel/img'+i.toString()+'.jpg')
+  for (var i = 1; i <= totalImages; i += 1) {
+    images.push('/img/imagesCarousel/img' + i.toString() + '.jpg')
   }
-  
+
   var lastTalk = Object.keys(talks).length - 1;
 
-  while(talks[lastTalk.toString()].video == null){
+  while (talks[lastTalk.toString()].video == null) {
     lastTalk -= 1;
   }
 
@@ -34,11 +33,11 @@ export default function Home(props) {
     <div>
       <Head>
         <title>GEOTOP-A</title>
-        <link rel="icon" href="/favicon.ico"/>
+        <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="initial-scale=1.0, maximum-scale=1" />
       </Head>
-      <HomePage {...props}/>
-      <br/>
+      <HomePage {...props} />
+      <br />
     </div>
   )
 }
