c793fdcce1452d2ecb29f667cc99c864b6713d0f
Miguel Ruiz <miguelruiz98@outlook.com>
2022-11-04 15:12:21 -0600

    fix: change of streaming time because of summer hour

--- components/StreamingTime/StreamingTimeSection.js
+++ components/StreamingTime/StreamingTimeSection.js
@@ -9,25 +9,25 @@ import Times from "../Clock/Times.js";
 
 const useStyles = makeStyles(styles);
 
-export default function StreamingTime(){
+export default function StreamingTime() {
     const classes = useStyles();
 
     const UTCMexicoCity = 360;
-    const StreamingTimeMexicoCity = 540; //antes del cambio de horario de verano: 600 - despues del cambio de horario de verano: 540
+    const StreamingTimeMexicoCity = 600; //antes del cambio de horario de verano: 600 - despues del cambio de horario de verano: 540
 
     const d = new Date();
     const localOffset = d.getTimezoneOffset();
 
-    const StreamingTimeLocal = (StreamingTimeMexicoCity + UTCMexicoCity - localOffset + 1440)%1440;
+    const StreamingTimeLocal = (StreamingTimeMexicoCity + UTCMexicoCity - localOffset + 1440) % 1440;
 
-    return(
-        <div style={{paddingLeft: "10%", paddingRight:"10%", paddingTop:"5%", color: "#fff"}} justify = "left">
+    return (
+        <div style={{ paddingLeft: "10%", paddingRight: "10%", paddingTop: "5%", color: "#fff" }} justify="left">
             <div>
                 <h1 className={classes.title}>Streaming time</h1>
             </div>
 
             <div>
-                <Times 
+                <Times
                     timeLabel={"CDMX time"}
                     timeLeftInMinutes={StreamingTimeLocal}
                     color='#282c34'
