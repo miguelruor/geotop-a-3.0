40ff316fa40e6738e7f6ca37c43c49101c88ef28
Miguel Ruiz <miguelruiz98@outlook.com>
2023-01-04 12:06:38 -0600

    chore: cleaning

--- pages/list-keywords.js
+++ pages/list-keywords.js
@@ -3,76 +3,62 @@ import SearchByKeywordPage from '../views/Search/ByKeywordPage/ByKeywordPage';
 import speakers from '../data/speakers.json';
 import talks from '../data/talks.json';
 
-export async function getStaticProps(){
-  
+export async function getStaticProps() {
+
   var keywords = {};
   let letterSet = new Set(); // Set con primeras letras de keywords en mayusculas
   let visitLetters = {}; // 
-  let keywordsWithLetter = {}; 
+  let keywordsWithLetter = {};
   // keywordsWithLetter[a] = [{keyword1_con_a: [talk1_id, talk2_id, ...]}, 
   // {keyword2_con_a: [talk3_id, ...]}, ...]
-
-  var month = new Array();
-  month[0] = "January";
-  month[1] = "February";
-  month[2] = "March";
-  month[3] = "April";
-  month[4] = "May";
-  month[5] = "June";
-  month[6] = "July";
-  month[7] = "August";
-  month[8] = "September";
-  month[9] = "October";
-  month[10] = "November";
-  month[11] = "December";
 
   var talks_ext = {}
 
   Object.keys(talks).forEach(talk_id => {
-    
-    var date = talks[talk_id].date2;
-    date = new Date(date.slice(0, 4), date.slice(4, 6)-"01", date.slice(6,8));
 
-    if(date > new Date()){
+    var date = talks[talk_id].date2;
+    date = new Date(date.slice(0, 4), date.slice(4, 6) - "01", date.slice(6, 8));
+
+    if (date > new Date()) {
       return;
     }
-    
+
     let keys = talks[talk_id].keywords;
     let keys_len = keys.length;
 
-    for(let i=0; i<keys_len; i++){
-        // Checo si encuentro una keyword nueva
-        if(!(keys[i] in keywords)){
-            keywords[keys[i]] = []
-        }
-        keywords[keys[i]].push(talk_id); 
+    for (let i = 0; i < keys_len; i++) {
+      // Checo si encuentro una keyword nueva
+      if (!(keys[i] in keywords)) {
+        keywords[keys[i]] = []
+      }
+      keywords[keys[i]].push(talk_id);
     }
 
     var idx = talks[talk_id].speaker_id.toString();
     var year = date.getFullYear();
     var stringDate = talks[talk_id].date;
-    
+
     talks_ext[talk_id] = {
-        ...talks[talk_id],
-        surname: speakers[idx].surname,
-        speaker: speakers[idx].completeName,
-        year: year,
-        date: stringDate
+      ...talks[talk_id],
+      surname: speakers[idx].surname,
+      speaker: speakers[idx].completeName,
+      year: year,
+      date: stringDate
     };
   })
 
   // Función que revisa las letras que existen para hacer listas  
-  for(var k in keywords){
-      var letter = k.charAt(0).toUpperCase();
-      letterSet.add(letter);
-      keywordsWithLetter[letter] = [];
+  for (var k in keywords) {
+    var letter = k.charAt(0).toUpperCase();
+    letterSet.add(letter);
+    keywordsWithLetter[letter] = [];
   }
 
-  for(var k in keywords){
-      var letter = k.charAt(0).toUpperCase() ;
-      var copy = {};
-      copy[k] = keywords[k];
-      keywordsWithLetter[letter].push(copy);
+  for (var k in keywords) {
+    var letter = k.charAt(0).toUpperCase();
+    var copy = {};
+    copy[k] = keywords[k];
+    keywordsWithLetter[letter].push(copy);
   }
   var auxLetterSet = [...letterSet]; // convertir a lista el set 
   auxLetterSet.sort(); // ordenar
@@ -80,8 +66,8 @@ export async function getStaticProps(){
   var lettersInKeywords = [...letterSet]; // convertir a lista el set 
   lettersInKeywords.sort(); // ordenar
 
-  return{
-    props:{
+  return {
+    props: {
       keywords: keywords,
       keywordsListByLetter: keywordsWithLetter,
       lettersInKeywords: lettersInKeywords,
@@ -95,10 +81,10 @@ export default function SearchByKeyword(props) {
     <div>
       <Head>
         <title>GEOTOP-A</title>
-        <link rel="icon" href="/favicon.ico"/>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <SearchByKeywordPage  keywords={props.keywords} keywordsListByLetter={props.keywordsListByLetter}
-               lettersInKeywords={props.lettersInKeywords} talks= {props.talks}/>
+      <SearchByKeywordPage keywords={props.keywords} keywordsListByLetter={props.keywordsListByLetter}
+        lettersInKeywords={props.lettersInKeywords} talks={props.talks} />
     </div>
   )
 }
