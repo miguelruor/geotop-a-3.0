2a4e788e187a06905a1ffb8e515a4e5726d30c5c
miguelruor <miguelruiz98@outlook.com>
2023-02-16 22:54:40 -0600

    feat: faqs page

--- pages/[meeting]/faqs-and-suggestions.js
+++ pages/[meeting]/faqs-and-suggestions.js
@@ -1,36 +1,17 @@
-import Background from "../../components/Meetings/Background/background";
-import Typography from '@mui/material/Typography';
+import Background from '../../components/Proceedings/Background/Background';
+import { makeStyles } from "@material-ui/core/styles";
+import styles from "../../views/HomePage/HomePageStyle.js";
 
-
-export default function MeetingPage(props) {
-    return (
-        <Background {...props}>
-            <Typography paragraph>
-                FAQs and Suggestions
-            </Typography>
-            <Typography paragraph>
-                Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-                eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-                neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-                tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-                sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-                tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-                gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-                et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-                tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-                eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-                posuere sollicitudin aliquam ultrices sagittis orci a.
-            </Typography>
-        </Background>
-    )
-}
+const useStyles = makeStyles(styles);
 
 export async function getStaticProps(context) {
     const meeting = context.params.meeting;
+
     return {
         props: {
             meeting: meeting,
-            backgroundColor: "#a7a7a7"
+            meetingTitle: "Seminar GEOTOP-A",
+            shortDescription: "First presential meeting in Merida, Mexico"
         }
     }
 }
@@ -45,4 +26,32 @@ export async function getStaticPaths() {
         paths: paths,
         fallback: false
     };
+}
+
+export default function AbstractSubmission(props) {
+
+    const classes = useStyles();
+
+    return (
+        <Background title={props.meetingTitle} meetingId={props.meeting} shortDescription={props.shortDescription}>
+            <h1 className={classes.paragraphTitle}>FAQs and Suggestions</h1>
+            <br />
+            <p>
+                <b>Q.: How do I find the driver of the shuttle to the Majorana Centre at the airport?</b>
+                <br />
+                A.: Just outside the baggage claim (either at Palermo or Trapani airport) look for a driver with a plate bearing the name of the Majorana Centre and the event you are attending. The driver is instructed to collect a group of participants at given time slots during the whole day of September 1.
+            </p>
+            <p>
+                <b>Q.: Can I get a transfer to/from the Majorana Centre any other day during the workshop?</b>
+                <br />
+                A.: Yes, just contact the Centre directly to organize the transfer. Call the local number 0923 869 133. The service is NOT free of charge and for this you should pay the Centre (ask for an invoice if you need reimbursement).
+            </p>
+            <p>
+                <b>Q.: What do I do in case of need or help?</b>
+                <br />
+                A.: Call the Majorana Centre; dial the local number 0923 869 133 (keep this number with you at all times).
+            </p>
+
+        </Background>
+    )
 }
