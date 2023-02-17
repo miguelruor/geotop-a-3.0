13b8cf0bfeecaea2b8107434c06fafa72ed1477a
miguelruor <miguelruiz98@outlook.com>
2023-02-16 22:02:20 -0600

    feat: abstract submissions page

--- pages/[meeting]/abstract-submission.js
+++ pages/[meeting]/abstract-submission.js
@@ -1,7 +1,13 @@
+import { Fragment } from 'react';
+
 import Background from '../../components/Proceedings/Background/Background';
 import { makeStyles } from "@material-ui/core/styles";
-import Divider from '@material-ui/core/Divider';
 import styles from "../../views/HomePage/HomePageStyle.js";
+
+import Hidden from '@material-ui/core/Hidden';
+import GridContainer from "../../components/Grid/GridContainer.js";
+import GridItem from "../../components/Grid/GridItem.js";
+import Divider from '@material-ui/core/Divider';
 
 // nodejs library that concatenates classes
 
@@ -12,7 +18,39 @@ export async function getStaticProps(context) {
 
     return {
         props: {
-            meeting: meeting
+            meeting: meeting,
+            meetingTitle: "Seminar GEOTOP-A",
+            shortDescription: "First presential meeting in Merida, Mexico",
+            submissions: [
+                {
+                    speaker_num: 1,
+                    speaker: "ALEXANDER Gareth",
+                    link: "http://www.renzoricca.com/Erice/abstracts/ALEXANDER.pdf",
+                    presentation_type: "Keynote, 1hr",
+                    email: "G.P.Alexander@warwick.ac.uk",
+                },
+                {
+                    speaker_num: 2,
+                    speaker: "BALE Arron",
+                    link: "http://www.renzoricca.com/Erice/abstracts/BALE.pdf",
+                    presentation_type: "30 mins",
+                    email: "arron.n.bale@durham.ac.uk",
+                },
+                {
+                    speaker_num: 3,
+                    speaker: "BARENGHI Carlo",
+                    link: "http://www.renzoricca.com/Erice/abstracts/BARENGHI.pdf",
+                    presentation_type: "Keynote, 1hr",
+                    email: "Carlo.barenghi@newcastle.ac.uk",
+                },
+                {
+                    speaker_num: 4,
+                    speaker: "BELLONI Andrea",
+                    link: "http://www.renzoricca.com/Erice/abstracts/BELLONI.pdf",
+                    presentation_type: "30 mins",
+                    email: "a.belloni17@outlook.it",
+                }
+            ]
         }
     }
 }
@@ -31,43 +69,64 @@ export async function getStaticPaths() {
 
 export default function AbstractSubmission(props) {
 
+    const { submissions } = props;
+
     const classes = useStyles();
 
     return (
-        <Background title="Seminar GEOTOP-A" meetingId={props.meeting} shortDescription="First presential meeting in Merida, Mexico">
-            <div className={classes.paragraphTitle}>
-                <h1>Keynote Speakers</h1>
-            </div>
-            <ul>
-                <li>Gareth P Alexander (U Warwick)</li>
-                <li>Carlo Barenghi (Newcastle U)</li>
-                <li>Mitchell A Berger (U Exeter)</li>
-                <li>Jason Cantarella (U Georgia)</li>
-                <li>Mark Dennis (U Birmingham)</li>
-                <li>Yasuhide Fukumoto (Kyushu U)</li>
-                <li>Gunnar Hornig (U Dundee)</li>
-                <li>Randall Kamien (U Pennsylvania)</li>
-                <li>Louis H Kauffman (U Illinois at Chicago)</li>
-                <li>Boris A. Khesin (U. Toronto)</li>
-                <li>Sofia Lambropoulou (Technical U. Athens)</li>
-                <li>Xin Liu (Beijing U Technology)</li>
-                <li>David MacTaggart (Glasgow U)</li>
-                <li>Vassily Manturov (Moscow Institute of Physics and Technology)</li>
-                <li>Sergei Nechaev (U Paris-Saclay)</li>
-                <li>Antti Niemi (NORDITA Stockholm)</li>
-                <li>Daniel Peralta-Salas (ICMAT Madrid)</li>
-                <li>Renzo L. Ricca (U Milano-Bicocca)</li>
-                <li>Rob Scharein (Hypnagogic Software)</li>
-                <li>Urs Schreiber (New York U at Abu Dhabi)</li>
-                <li>Koya Shimokawa (Ochanomizu U)</li>
-                <li>Clayton Shonkwiler (Colorado State U)</li>
-                <li>De Witt L Sumners (Florida State University)</li>
-                <li>Paul M. Sutcliffe (U Durham)</li>
-                <li>Jean-Luc Thiffeault (U Wisconsin at Madison)</li>
-                <li>Alexander Voronov (U Minnesota)</li>
-                <li>Asher Yahalom (Ariel U)</li>
-                <li>Anthony Yeates (Durham U)</li>
-            </ul>
+        <Background title={props.meetingTitle} meetingId={props.meeting} shortDescription={props.shortDescription}>
+            <h1 className={classes.paragraphTitle}>Abstract Submission</h1>
+            <br />
+            <h2>Lectures</h2>
+            <GridContainer>
+                <Hidden only="xs">
+                    <GridItem sm={1}>
+                        <h4>#</h4>
+                    </GridItem>
+                    <GridItem sm={3}>
+                        <h4>Name</h4>
+                    </GridItem>
+                    <GridItem sm={4}>
+                        <h4>Presentation type</h4>
+                    </GridItem>
+                    <GridItem sm={4}>
+                        <h4>Email</h4>
+                    </GridItem>
+                </Hidden>
+                {submissions.map(talk => (
+                    <Fragment key={talk.speaker}>
+                        <Hidden xsDown>
+                            <GridItem sm={1}>
+                                <p>{talk.speaker_num}</p>
+                            </GridItem>
+                            <GridItem sm={3}>
+                                <p><a href={talk.link} target="_blank">{talk.speaker}</a></p>
+                            </GridItem>
+                            <GridItem sm={3}>
+                                <p>{talk.presentation_type}</p>
+                            </GridItem>
+                            <GridItem sm={4}>
+                                <p>{talk.email}</p>
+                            </GridItem>
+                            <GridItem sm={12}>
+                                <Divider variant="fullWidth" />
+                            </GridItem>
+                        </Hidden>
+                        {/* Seccion Movil */}
+                        <Hidden smUp>
+                            <GridItem xs={12}>
+                                <p><a href={talk.link} target="_blank">{talk.speaker}</a> - {talk.presentation_type}</p>
+                            </GridItem>
+                            <GridItem xs={12}>
+                                <p>{talk.email}</p>
+                            </GridItem>
+                            <GridItem xs={12}>
+                                <Divider variant="fullWidth" />
+                            </GridItem>
+                        </Hidden>
+                    </Fragment>
+                ))}
+            </GridContainer>
         </Background>
     )
 }
