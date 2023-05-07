39260f913fa91c320b39ce7db43ef058025adb0f
miguelruor <miguelruiz98@outlook.com>
2023-05-07 03:32:10 -0600

    feat: responsive images for merida's meeting

--- pages/[meeting]/venue-and-travel-infos.js
+++ pages/[meeting]/venue-and-travel-infos.js
@@ -1,6 +1,7 @@
 import Background from '../../components/Proceedings/Background/Background';
 import style from "../../assets/css/meetings.module.css";
 import Image from 'next/image';
+import Hidden from '@material-ui/core/Hidden';
 
 import meetingData from "../../data/meeting.json";
 
@@ -28,14 +29,19 @@ export default function AbstractSubmission(props) {
         <Background title={props.meetingTitle} meetingId={props.meetingId} shortDescription={props.shortDescription}>
             <h1 className={style.paragraphTitle}>Venue and Travel Infos</h1>
             <h2>Yucatán</h2>
-            <div style={{ display: "flex", alignItems: "center" }}>
-                <div style={{ width: "60%", paddingRight: "30px" }}>
+            <div className={style.imageAndText}>
+                <div className={style.sideImageText}>
                     <p>
                         México is amongst the most popular holiday destinations in the world, and Yucatán is arguably
                         one of its most exciting states to visit, with some of México’s most popular tourist attractions and
                         destinations: from Mérida to Chichen Itza, from Cancún to Playa Del Carmen, Tulum, Cozumel
                         and many other.
                     </p>
+                    <Hidden mdUp>
+                        <div className={style.centeredImage}>
+                            <Image src="/img/meetings/merida24/others/maya.jpg" objectFit='contain' width={500} height={350} />
+                        </div>
+                    </Hidden>
                     <p>
                         Click <a href="https://drive.google.com/file/d/16NyLjacUX-zxw_vS3whVJNEDHpua2J1h/view?usp=share_link" target='_blank'>HERE</a> to see the key
                         places and the typical driving times on the map.
@@ -45,29 +51,35 @@ export default function AbstractSubmission(props) {
                         For a guide to some of its most beautiful beaches click <a href="https://traveltomerida.com/best-merida-mexico-beaches-in-yucatan/" target='_blank'>HERE</a>.
                     </p>
                 </div>
-                <div style={{ width: "35%", paddingLeft: "30px" }}>
-                    <Image src="/img/meetings/merida24/others/maya.jpg" width={500} height={350} />
-                </div>
+                <Hidden smDown className={style.sideImage}>
+                    <Image src="/img/meetings/merida24/others/maya.jpg" objectFit='contain' width={500} height={400} />
+                </Hidden>
             </div>
 
             <h2>Mérida</h2>
-            <div style={{ display: "flex", alignItems: "center" }}>
-                <div style={{ width: "60%", paddingRight: "30px" }}>
+            <div className={style.imageAndText}>
+                <div className={style.sideImageText}>
                     <p>
                         This is the capital city of Yucatán, about 4 hours driving from Cancún International Airport and about
                         40 minutes driving from Progreso and the nearby coastline beaches (click <a href="https://drive.google.com/file/d/16NyLjacUX-zxw_vS3whVJNEDHpua2J1h/view?usp=share_link" target='_blank'>HERE</a> to see the
                         typical driving times on the map).
                     </p>
+                    <Hidden mdUp>
+                        <div className={style.centeredImage}>
+                            <Image src="/img/meetings/merida24/others/Merida.png" objectFit='contain' width={500} height={350} />
+                        </div>
+                    </Hidden>
                     <p>
                         Mérida is a city of about 1.1 million inhabitants. The Autonomous University of Yucatán (UADY) and the
                         conference hall located in the city centre are at a walking distance from the major tourist attractions,
                         hotels and night life places. Click <a href="https://drive.google.com/file/d/1PoLkC44Z13b0GNKHrT7OUwKDlGzQuqa2/view?usp=share_link" target='_blank'>HERE</a> to see the local map.
                     </p>
                 </div>
-
-                <div style={{ width: "35%", paddingLeft: "30px" }}>
-                    <Image src="/img/meetings/merida24/others/Merida.png" width={500} height={350} />
-                </div>
+                <Hidden smDown>
+                    <div className={style.sideImage}>
+                        <Image src="/img/meetings/merida24/others/Merida.png" objectFit='contain' width={500} height={400} />
+                    </div>
+                </Hidden>
             </div>
 
 
@@ -78,12 +90,6 @@ export default function AbstractSubmission(props) {
             </p>
 
             <h2>How to get to Mérida from Cancún International Airport</h2>
-            {/*<p>
-                Major international airlines as well as charter companies have direct flights into Cancún
-                International Airport from all over the world, and from more than 40 cities in the United States
-                and more than 20 cities in Europe. You can check some key information on operating airlines
-                and flights <a href="https://www.cancunairport.com/" target='_blank'>HERE</a>.
-            </p>*/}
             <p>
                 The overland connection to Mérida by car is a journey
                 that varies from 4 hours, by a hired private shuttle
@@ -98,8 +104,8 @@ export default function AbstractSubmission(props) {
                 click <a href="https://drive.google.com/file/d/1i53y2mYIvCA-YLX9JD8wyEMobrehWcCb/view?usp=share_link" target="_blank">HERE</a> to see
                 a reference table in US dollars.
             </p>
-            <div style={{ display: "flex", alignItems: "center" }}>
-                <div style={{ width: "55%", paddingRight: "30px" }}>
+            <div className={style.imageAndText}>
+                <div className={style.sideImageText}>
                     <p>
                         There are a few direct flights from US and Canadian cities to Merida (Dallas, Houston, Miami, Toronto, ...).
                         A fast train connection provided by the <a href="https://www.alstom.com/mayan-train-project" target="_blank">Mayan Train</a> between the cities of Mérida
@@ -109,10 +115,17 @@ export default function AbstractSubmission(props) {
                         kilometres of train line. If available, this would be a
                         very good solution.
                     </p>
+                    <Hidden mdUp>
+                        <div className={style.centeredImage}>
+                            <Image src="/img/meetings/merida24/others/MayaTrain_map.jpg" objectFit='contain' width={700} height={350} />
+                        </div>
+                    </Hidden>
                 </div>
-                <div style={{ width: "45%", paddingLeft: "30px" }}>
-                    <Image src="/img/meetings/merida24/others/MayaTrain_map.jpg" width={700} height={350} />
-                </div>
+                <Hidden smDown>
+                    <div className={style.sideImage}>
+                        <Image src="/img/meetings/merida24/others/MayaTrain_map.jpg" objectFit='contain' width={700} height={400} />
+                    </div>
+                </Hidden>
             </div>
 
         </Background>
