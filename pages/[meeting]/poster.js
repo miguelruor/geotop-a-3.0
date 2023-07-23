b25a74f1bc39199dcee21d93ab4d56d0eaa77256
miguelruor <miguelruiz98@outlook.com>
2023-07-23 16:02:29 -0600

    fix(merida24): fix size of poster

--- pages/[meeting]/poster.js
+++ pages/[meeting]/poster.js
@@ -31,7 +31,8 @@ export default function AbstractSubmission(props) {
 
     return (
         <Background title={props.meetingTitle} meetingId={props.meeting} shortDescription={props.shortDescription}>
-            <span style={{ display: "flex", alignItems: "center" }}><h1 className={style.paragraphTitle}>Poster </h1><DownloadButton /></span>
+            <h1 className={style.paragraphTitle}>Poster </h1>
+            <DownloadButton />
 
             <div className={style.poster}>
                 <Image src="/img/meetings/merida24/poster.jpeg" objectFit='contain' layout="fill" />
@@ -42,6 +43,16 @@ export default function AbstractSubmission(props) {
 
 const DownloadButton = () => {
     return <a href='https://drive.google.com/file/d/1jbc5hgU2bfN88ibyIhDHshFrxKYBYHeM/view?usp=drive_link' target='_blank'>
-        <DownloadIcon fontSize='large' style={{ marginLeft: "25px", border: "1px solid rgba(0, 0, 0, 0.1)", padding: "5px", boxShadow: "0px 8px 15px rgba(0, 0, 0, 0.1)" }} />
+        <DownloadIcon
+            fontSize='large'
+            style={{
+                border: "1px solid rgba(0, 0, 0, 0.1)",
+                padding: "5px",
+                boxShadow: "0px 8px 15px rgba(0, 0, 0, 0.1)",
+                position: "absolute",
+                top: "70px",
+                right: "30px"
+            }}
+        />
     </a>
 }
