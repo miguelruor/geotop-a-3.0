e474e3d4971b1f0ad5d165c2402286909d774a39
miguelruor <miguelruiz98@outlook.com>
2024-01-17 12:35:24 -0600

    fix: quit Merida's talks announcement

--- views/NextTalksPage/NextTalksPage.js
+++ views/NextTalksPage/NextTalksPage.js
@@ -48,86 +48,10 @@ export default function NextTalksPage(props) {
             props.next_talks.map((talk, i) => (<TalkLayout key={i} {...talk} />))
             :
             <></>}
-        <div className={classes.containerContent} style={{ color: "black", paddingBottom: "10px" }}>
-          <div style={{ paddingRight: "40px" }}>
-            <h2 style={{ textAlign: "center" }}>
-              Keynote talks at GEOTOP-A Conference - January 8-13, 2024.
-            </h2>
-            <h2 style={{ textAlign: "center" }}>
-              Mérida, Yucatán, México
-            </h2>
-            <p>
-              See abstracts and transmissions links on the <Link href={"/merida24/scientific-programme"}>Scientific Programme</Link>.
-            </p>
-            <p>
-              Schedule in CDMX time (GMT-6).
-            </p>
-            <p>
-              <ul>
-                <li style={{ marginBottom: "10px" }}>
-                  Monday January 8. 9:00 - 10:00 am. <br />
-                  Mitchell Berger - University of Exeter <br />
-                  <b>Continuous topological measures: helicity, winding, and higher order winding</b> <br />
-                </li>
-                <li style={{ marginBottom: "10px" }}>
-                  Monday January 8. 3:00 - 4:00 pm. <br />
-                  Cristian Micheletti - International School for Advanced Studies (SISSA) <br />
-                  <b>Dynamics and mechanics of knotted DNA and RNAs: insights from molecular dynamics simulations</b> <br />
-                </li>
-                <li style={{ marginBottom: "10px" }}>
-                  Monday January 8. 6:00 - 7:00 pm. <br />
-                  Yuliy Baryshnikov - University of Illinois at Urbana-Champaign <br />
-                  <b>On Spaces of Coverings</b> <br />
-                </li>
-                <li style={{ marginBottom: "10px" }}>
-                  Tuesday January 9. 9:00 - 10:00 am. <br />
-                  Ismar Volić - Wellesley College <br />
-                  <b>Simplicial complexes and political structures</b> <br />
-                </li>
-                <li style={{ marginBottom: "10px" }}>
-                  Tuesday January 9. 3:00 - 4:00 pm. <br />
-                  Pablo Soberón - City University of New York <br />
-                  <b>New results on envy-free distributions</b> <br />
-                </li>
-                <li style={{ marginBottom: "10px" }}>
-                  Wednesday January 10. 9:00 - 10:00 am. <br />
-                  Alexander Dranishnikov - University of Florida <br />
-                  <b>On some variations of TC and the LS-category</b> <br />
-                </li>
-                <li style={{ marginBottom: "10px" }}>
-                  Thursday January 11. 9:00 - 10:00 am. <br />
-                  Sophie Jackson - University of Cambridge <br />
-                  <b>The What, Where, How and Why of Topological Knots in Proteins</b> <br />
-                </li>
-                <li style={{ marginBottom: "10px" }}>
-                  Thursday January 11. 3:00 - 4:00 pm. <br />
-                  Louis H Kauffman - University of Illinois at Chicago <br />
-                  <b>Reconnection Numbers of Knotted Vortices</b> <br />
-                </li>
-                <li style={{ marginBottom: "10px" }}>
-                  Friday January 12. 9:00 - 10:00 am. <br />
-                  Yusu Wang - UC San Diego <br />
-                  <b>Graph learning models: theoretical understanding, limitations and enhancements</b> <br />
-                </li>
-                <li style={{ marginBottom: "10px" }}>
-                  Saturday January 13. 9:00 - 10:00 am. <br />
-                  Radmila Sazdanovic - NC State University <br />
-                  <b>The shape of relations: knots and other stories</b> <br />
-                </li>
-                <li style={{ marginBottom: "10px" }}>
-                  Saturday January 13. 5:00 - 6:00 pm. <br />
-                  Pawel Dlotko - Dioscuri Centre in Topological Data Analysis, Mathematical Institute, Polish Academy of Sciences <br />
-                  <b>Data, their shape, and beyond</b> <br />
-                </li>
 
-              </ul>
-            </p>
-          </div>
-        </div>
-
-        {/*<div className={classes.container}>
+        <div className={classes.container}>
           <StreamingTimeSection />
-        </div>*/}
+        </div>
       </div>
       <Footer />
 
