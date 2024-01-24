638b53742fef19f58252fe52561e3538a2283fb4
miguelruor <miguelruiz98@outlook.com>
2024-01-24 12:46:36 -0600

    feat(merida24): add photos

--- pages/[meeting]/group-photo-and-gallery.js
+++ pages/[meeting]/group-photo-and-gallery.js
@@ -1,5 +1,9 @@
+import { useEffect, useState } from 'react';
 import Background from '../../components/Proceedings/Background/Background';
 import style from "../../assets/css/meetings.module.css";
+import Image from 'next/image';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
 
 export async function getStaticProps(context) {
     const meeting = context.params.meeting;
@@ -12,6 +16,42 @@ export async function getStaticProps(context) {
         }
     }
 }
+
+const itemData = [
+    "Angulo Medium.jpeg",
+    "Berger Medium.jpeg",
+    "Bernardo Jose Medium.jpeg",
+    "Cristian M Medium.jpeg",
+    "Cristian Renzo Medium.jpeg",
+    "Eric GEOTOP A Medium.jpeg",
+    "Eric and Sophia Medium.jpeg",
+    "Fosado Medium.jpeg",
+    "Gilda Esteban Medium.jpeg",
+    "Group 1 Medium.jpeg",
+    "IMG_0321 Medium.jpeg",
+    "IMG_0331 Medium.jpeg",
+    "IMG_0334 Medium.jpeg",
+    "IMG_0346 Medium.jpeg",
+    "IMG_0352 Medium.jpeg",
+    "IMG_0368 Medium.jpeg",
+    "JL Leon Medium.jpeg",
+    "Joaquin estudiantes Medium.jpeg",
+    "Ma Fernanda Marlene Medium.jpeg",
+    "N1 Medium.jpeg",
+    "N2 Medium.jpeg",
+    "Natasha Medium.jpeg",
+    "Pawel Medium.jpeg",
+    "Pawel1 Medium.jpeg",
+    "Rogelio Jesus Medium.jpeg",
+    "Sabina Medium.jpeg",
+    "Social Grupal Medium.jpeg",
+    "Sophia K Medium.jpeg",
+    "Sophie social Medium.jpeg",
+    "Sophie Medium.jpeg",
+    "Virk Tombari Medium.jpeg",
+    "Volic Medium.jpeg",
+    "jason Medium.jpeg",
+];
 
 
 export async function getStaticPaths() {
@@ -26,20 +66,30 @@ export async function getStaticPaths() {
 }
 
 export default function GroupPhotoAndGallery(props) {
+    const [numCols, setNumCols] = useState(1)
+
+    useEffect(() => {
+        // window is accessible here.
+        setNumCols(window.innerWidth < 1000 ? 1 : 3);
+        console.log("hey there")
+    }, []);
 
     return (
         <Background title={props.meetingTitle} meetingId={props.meeting} shortDescription={props.shortDescription}>
-            <h1 className={style.paragraphTitle}>Group Photo and Gallery</h1>
-            <h2>TBA</h2>
-            {/*
-            <br />
+            <h1 className={style.paragraphTitle}>Group Photos and Gallery</h1>
+
             <p>
-                A high-resolution Group Photo, taken on Sunday September 4, can be downloaded from HERE.
+                Download them <a href="https://drive.google.com/drive/folders/1Cm3Hq2JMmkWOuk7WWgo5P7KWlLRj2G0h?usp=sharing" target="_blank">here</a>.
             </p>
-            <p>
-                The Group Photo complete with all the names of the participants present either in person or virtually on-line (shown here below) can be downloaded as a PDF file (4.9 MB) HERE, or as a JPG file (1 MB) HERE.
-            </p>
-    */}
+
+            <ImageList sx={{ width: "100%", height: 800 }} cols={numCols} >
+                {itemData.map((item) => (
+                    <ImageListItem key={item}>
+                        <Image src={`/img/meetings/merida24/photos/${item}`} objectFit='contain' width={1000} height={1000} loading="lazy" />
+                    </ImageListItem>
+                ))}
+            </ImageList>
+
         </Background>
     )
 }
