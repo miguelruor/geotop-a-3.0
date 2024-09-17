dc1c96115a9aa5078fb191a9eab2f11b83e4e236
miguelruor <miguelruiz98@outlook.com>
2024-09-16 19:13:32 -0600

    fix: banner with better dimensions

--- views/HomePage/HomePage.js
+++ views/HomePage/HomePage.js
@@ -2,6 +2,8 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import HeaderLinks from "../../components/Header/HeaderLinks";
 import Parallax from "../../components/Parallax/Parallax.js";
+import Button from "../../components/CustomButtons/Button.js";
+import Link from "next/link"
 import { makeStyles } from "@material-ui/core/styles";
 import styles from "./HomePageStyle.js";
 
@@ -33,16 +35,22 @@ export default function HomePage(props) {
         }}
       />
 
-      <Parallax home style={
-        {
-          backgroundSize: "contain",
-          backgroundRepeat: "no-repeat",
-          alignItems: "flex-end",
-          backgroundPosition: "center 70px"
-        }
-      } image={backgroundImageHome} />
+      <Parallax home image={backgroundImageHome} />
 
       <div className={classNames(classes.main, classes.mainRaised)}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: "20px 0px" }}>
+          <Link href={"previous-talks/" + props.last_talk}>
+            <Button
+              color="primary"
+              size="lg"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fas fa-play" />
+              Watch our last seminar!
+            </Button>
+          </Link>
+        </div>
         <SectionCarousel images={props.images} />
         <FutureTalksSection />
         <StreamingTimeSection />
